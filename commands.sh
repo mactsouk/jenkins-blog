a068eae2089269a2249a328993e08092ecedfcee
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-04-01 20:44:51 +0300

    Commands in script

---
+++ commands.sh
@@ -0,0 +1,5 @@
+uname -a
+wget https://github.com/lensesio/lenses-go/releases/download/3.1.0/lenses-cli-linux-386.tar.gz
+tar xzvf lenses-cli-linux-386.tar.gz
+mv lenses-cli-linux-386 lenses-cli
+./lenses-cli --host http://dc703fdc.ngrok.io  --pass admin --user admin topics
