37e7a33562faa7b640d56f03f1cf50d93fa1275b
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-04-03 20:23:31 +0300

    Minor change

--- commands.sh
+++ commands.sh
@@ -3,3 +3,4 @@ wget --quiet https://github.com/lensesio/lenses-go/releases/download/3.1.0/lense
 tar xzvf lenses-cli-linux-386.tar.gz
 mv lenses-cli-linux-386 lenses-cli
 ./lenses-cli --host http://a00474c0.ngrok.io  --pass admin --user admin topics
+
